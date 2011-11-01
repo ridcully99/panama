66e0a39707755d8f002d3b4236fecec3ff63ec7d
robert.brandner@gmail.com <robert.brandner@gmail.com@11ccd14b-a046-9c1e-6379-9f16e458a607>
2011-11-01 16:49:06 +0000

    Berechnung von Ants in Hive eingebaut und von Defense Mechanismus. Berechnung von knownTerritory eingebaut, rudimentär, noch nicht genutzt. Next: nur 1 zu food schicken

--- ants/MyBot.java
+++ ants/MyBot.java
@@ -6,7 +6,7 @@ import java.util.Map;
 import java.util.Set;
 
 /**
- * Version 11
+ * Version 11 (online als Version 10)
  */
 public class MyBot extends Bot {
 	
@@ -24,23 +24,44 @@ public class MyBot extends Bot {
         new MyBot().readSystemInput();
     }
 
+    @Override
+    public void setup(int loadTime, int turnTime, int rows, int cols, int turns, int viewRadius2, int attackRadius2, int spawnRadius2) {
+    	super.setup(loadTime, turnTime, rows, cols, turns, viewRadius2, attackRadius2, spawnRadius2);
+    }
+    
+    @Override
+    public void removeAnt(int row, int col, int owner) {
+    	super.removeAnt(row, col, owner);
+    	if (owner == 0) { 
+    		antsDied++;
+    	}
+    }
+    
     private Map<Tile, Tile> longTermDestination = new HashMap<Tile, Tile>();	// ant --> destination
     
     private Ants ants;
     private Set<Tile> blocked = new HashSet<Tile>();
     private int turn = 0;
-    private int antsInHive = 0;
+    private int antsDied = 0;
+    private int foodEaten = 0;
+    private Set<Tile> foodLastTurn = new HashSet<Tile>();
     
     /**
      */
     @Override
     public void doTurn() {
-        ants = getAnts();
-        blocked.clear();
-        
+    	ants = getAnts();
+    	foodEaten += foodEatenLastTurn();
+    	int antsInHive = (1+foodEaten-antsDied)-ants.getMyAnts().size();	// wieviele es sein sollten - wieviele es sind
+    	int minAntsToKeepInHive = (int)Math.sqrt(ants.getMyAnts().size()) - 1;	// keep some ants in hive for safety (to defend hill)
+    	blocked.clear();
+        log("known territory: "+((ants.knownTerritory.size()*100f)/(ants.getCols()*ants.getRows()))+"%");
         for (Tile myAnt : ants.getMyAnts()) {
-        	if (isNextToFood(myAnt)) {
-        		continue;	// fressen lassen
+        	// defense
+        	if (ants.getMyHills().contains(myAnt) && 				// ant sitting on own hill
+        		antsInHive < minAntsToKeepInHive) {					// not enough ants in hive
+        		log("keep ant in hive, as we want "+minAntsToKeepInHive);
+        		continue;											// block hill until more ants in hive
         	}
         	Aim direction = findBestDirection(myAnt);
         	if (direction != null) {
@@ -62,19 +83,22 @@ public class MyBot extends Bot {
         turn++;
     }
 
-    private LinkedList<Tile> floodFillQueue = new LinkedList<Tile>();
+	private LinkedList<Tile> floodFillQueue = new LinkedList<Tile>();
     private Map<Tile, QueueData> traceBackData = new HashMap<Tile, QueueData>();
     
-    /* also detects if myAnt is directly next to food already - return is null so cannot be distinquished --> use isNextToFood() before */
     private Aim findBestDirection(Tile myAnt) {
+    	Tile mostUnknown = null;
+    	int maxUnknown = 0;
     	floodFillQueue.clear();
     	traceBackData.clear();
     	floodFillQueue.add(myAnt);
     	traceBackData.put(myAnt, new QueueData(null, null, 0));
     	Tile current = null;	// ausserhalb damit ich nachher das Tile das als letztes aus Queue genommen wurde, habe --> wenn kein Food dann in die Richtung gehen weil "am weitesten freie Bahn"
+    	queue:
     	while(!floodFillQueue.isEmpty() && ants.getTimeRemaining() >= ALMOST_TIMEOUT) {
     		current = floodFillQueue.pollFirst();
     		int currentDepth = traceBackData.get(current).steps;
+    		int currentUnknown = traceBackData.get(current).unknown;
     		if (currentDepth == 0 && current.equals(longTermDestination.get(myAnt))) {
     			log(myAnt+" has reached longterm destination; removing it.");
     			clearLongTermDestination(myAnt);
@@ -104,15 +128,26 @@ public class MyBot extends Bot {
             		return traceBack(dest, myAnt);
         		}
         		floodFillQueue.addLast(dest);
-        		traceBackData.put(dest, new QueueData(current, direction, currentDepth+1));
+        		int newUnknown = currentUnknown;
+        		if (!ants.knownTerritory.contains(dest)) {
+        			newUnknown += 1;
+        			if (newUnknown > maxUnknown) {
+        				maxUnknown = newUnknown;
+        				mostUnknown = dest;
+        			}
+        		}
+        		traceBackData.put(dest, new QueueData(current, direction, currentDepth+1, newUnknown));
      		}
     	}
     	// nichts besonderes gefunden.
-    	// Leftybot ist schlechter. return leftyMove(myAnt);
     	if (current != null) {	// current ist als letztes in Queue gewesen und somit am weitesten vom Ausgangspunkt weg == weitester freier Weg
 			// entweder hatte ant noch kein longtermdestination oder hat es nicht schon vorher gefunden (was eigentlich nur durch temporaere blockierungen passiert sein kann)
     		if (!longTermDestination.containsKey(myAnt)) {	// noch kein longtermdest --> eines setzen
-    			longTermDestination.put(myAnt, current);
+    			if (mostUnknown != null) {
+    				longTermDestination.put(myAnt, mostUnknown);
+    			} else {
+    				longTermDestination.put(myAnt, current);
+    			}
     			return traceBack(current, myAnt);
     		} else {
     			// hat schon eines, hat's aber via floodfill nicht erreicht... dont move, vielleicht gehts naechstesmal besser.
@@ -138,16 +173,30 @@ public class MyBot extends Bot {
  		longTermDestination.remove(ant);
  	}
  	
-	/* nicht diagonal */
-    private boolean isNextToFood(Tile ant) {
-    	for (Aim direction : Aim.values()) {
-    		if (ants.getIlk(ant, direction) == Ilk.FOOD) {
-    			return true;
+    private int foodEatenLastTurn() {
+    	int count = 0;
+    	foodLastTurn.removeAll(ants.getFoodTiles());	// was ueberbleibt war da und ist jetzt weg
+    	for (Tile food : foodLastTurn) {
+    		boolean mine = false;
+    		for (Aim direction : Aim.values()) {
+	    		if (ants.getIlk(food, direction) == Ilk.MY_ANT) {
+	    			mine = true;
+	    		}
+	    		if (ants.getIlk(food, direction) == Ilk.ENEMY_ANT) {
+	    			mine = false;
+	    			break;
+	    		}
+	    	}
+    		if (mine) {
+    			count++;
+    			log("I ate food at "+food+" last turn");
     		}
     	}
-    	return false;
-    }
-    
+    	foodLastTurn.clear();
+    	foodLastTurn.addAll(ants.getFoodTiles());
+    	return count;
+	}
+
 	private void log(Object s) {
 		if (LOGGING) {
 			System.err.println(turn+": "+s.toString());
