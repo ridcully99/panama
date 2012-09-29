41a735c08c1d507179b7d85e642fcdee2dfc62fd
robert.brandner@gmail.com <robert.brandner@gmail.com@11ccd14b-a046-9c1e-6379-9f16e458a607>
2012-09-29 15:59:49 +0000

    Fixed issue #31.

--- panama/src/panama/core/BaseController.java
+++ panama/src/panama/core/BaseController.java
@@ -1,17 +1,17 @@
 /*
- *  Copyright 2004-2010 Robert Brandner (robert.brandner@gmail.com) 
- *  
- *  Licensed under the Apache License, Version 2.0 (the "License"); 
- *  you may not use this file except in compliance with the License. 
- *  You may obtain a copy of the License at 
- *  
- *  http://www.apache.org/licenses/LICENSE-2.0 
- *  
- *  Unless required by applicable law or agreed to in writing, software 
- *  distributed under the License is distributed on an "AS IS" BASIS, 
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- *  See the License for the specific language governing permissions and 
- *  limitations under the License. 
+ *  Copyright 2004-2010 Robert Brandner (robert.brandner@gmail.com)
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 package panama.core;
 
@@ -40,23 +40,23 @@ import panama.util.TreeController;
 
 /**
  * Base class for all controllers.
- * 
+ *
  * @author Robert
  */
 public class BaseController {
 
 	/** Logging */
 	protected static SimpleLogger log = new SimpleLogger(BaseController.class);
-	
+
 	/** Current context for convinience */
 	protected final Context context = Context.getInstance();
-	
+
 	/**
 	 * This method is invoked every time, before an action of this controller is
 	 * invoked by the dispatcher.
 	 * Default implemention does nothing.
 	 * Useful to overwrite and put e.g. needed things into context or do authorization
-	 * 
+	 *
 	 * @param actionName The name of the action to be invoked later on
 	 * @throws ForceTargetException If necessary, this method may throw this exception to force a specific target (e.g. for checking login)
 	 * @throws AuthorizationException If necessary, this method may throw this exception to force a HTTP-Response of 403 (forbidden)
@@ -69,31 +69,31 @@ public class BaseController {
 	 * invoked by the dispatcher.
 	 * Default implementation does nothing.
 	 * Overwrite it if you need things to be done after most or all of your actions.
-	 * 
+	 *
 	 * @param actionName The name of the action to be invoked later on
 	 * @param target The result of the action
 	 * @throws ForceTargetException If necessary, this method may throw this exception to force a specific target (e.g. for checking login)
 	 */
 	public void afterAction(String actionName, Target target) throws ForceTargetException {
 	}
-	
+
 	/**
 	 * Defines a redirect to other url (relative or absolute).
 	 *
-	 * Sends a temporary redirect response to the client using the specified redirect location 
-	 * URL. This method can accept relative URLs; the servlet container must convert the 
-	 * relative URL to an absolute URL before sending the response to the client. 
-	 * If the location is relative without a leading slash (/) the container interprets it as 
-	 * relative to the current request URI. 
+	 * Sends a temporary redirect response to the client using the specified redirect location
+	 * URL. This method can accept relative URLs; the servlet container must convert the
+	 * relative URL to an absolute URL before sending the response to the client.
+	 * If the location is relative without a leading slash (/) the container interprets it as
+	 * relative to the current request URI.
 	 * If the location is relative with a leading slash (/) the container interprets it as relative to the webapp.
-	 * 
+	 *
 	 * To redirect to other action use /controller/action.now
-	 * 
+	 *
 	 * Note: Use redirect only if you need the client browser's URL to have a new URL.
-	 * 
+	 *
 	 * @param url
 	 * @return A Target object
-	 */	
+	 */
 	public Target redirect(String url) {
 		Target t = new RedirectTarget(url);
 		return t;
@@ -105,45 +105,45 @@ public class BaseController {
 	 * Sends a temporary redirect response to the client using a URL created from the
 	 * current Controller and the specified action and parameters
 	 * The URL has the form: ../controllerName/action?param1=value1&param2=value2...
-	 * 
+	 *
 	 * The parameter names and values are url-encoded by this method.
-	 * 
+	 *
 	 * Note: Use redirect only if you need the client browser's URL to have a new URL.
 	 *       Its <strong>much faster</strong> to use simply invoke the method of the required action and return it's result.
-	 * 
+	 *
 	 * @param action
 	 * @param optionalParamsAndValues an optional list of parameters and values, alternating like so: param1, value1, param2, value2...
 	 * @return A Target object
-	 */		
+	 */
 	public Target redirectToAction(String action, String... optionalParamsAndValues) {
 		return redirectToAction(this.getClass(), action, optionalParamsAndValues);
 	}
-	
+
 	/**
 	 * Defines a redirect to other action.
 	 *
 	 * Sends a temporary redirect response to the client using a URL created from the
 	 * specified controllerClass, action and the specified action and parameters
 	 * The URL has the form: ../controllerName/action?param1=value1&param2=value2...
-	 * 
+	 *
 	 * The parameter names and values are url-encoded by this method.
-	 * 
+	 *
 	 * Note: Use redirect only if you need the client browser's URL to have a new URL.
 	 *       Its <strong>much faster</strong> to use simply invoke the method of the required action and return it's result.
-	 * 
+	 *
 	 * @param controllerClass
 	 * @param action
 	 * @param optionalParamsAndValues an optional list of parameters and values, alternating like so: param1, value1, param2, value2...
 	 * @return A Target object
-	 */		
+	 */
 	public Target redirectToAction(Class<? extends BaseController> controllerClass, String action, String... optionalParamsAndValues) {
 		Map<Object, Object> parameterMap = buildParameterMap(optionalParamsAndValues);
 		return internalRedirectToAction(controllerClass, action, parameterMap);
 	}
-	
+
 	/**
 	 * Creates a TemplateTarget for the specified templateName (building absolute path is a relativ path was provided)
-	 * 
+	 *
 	 * @param templateName The name (with relative or absolute path) of the template to render
 	 * @return a Target object
 	 */
@@ -167,7 +167,7 @@ public class BaseController {
 		Target t = new TemplateTarget(templateName);
 		return t;
 	}
-	
+
 	/**
 	 * Executes an action of the same controller with an optional set of parameters.
 	 * After execution, the original parameters are restored.
@@ -201,13 +201,16 @@ public class BaseController {
 			parameterMap = new HashMap<Object, Object>();
 			for (int i=0; i<paramsAndValues.length; i+=2) {
 				Object key = paramsAndValues[i];
+				if (key == null) {
+					throw new IllegalArgumentException("parameter names must not be null");
+				}
 				Object value = i+1 < paramsAndValues.length ? paramsAndValues[i+1] : "";
 				parameterMap.put(key, value);
 			}
 		}
 		return parameterMap;
-	}	
-	
+	}
+
 	/**
 	 * Defines a redirect to other action.
 	 * This internal method is used by all the public redirectToAction() methods.
@@ -215,17 +218,17 @@ public class BaseController {
 	 * Sends a temporary redirect response to the client using a URL created from the
 	 * specified controllerClass, action and parameters.
 	 * The URL has the form: ../controllerName/action?param1=value1&param2=value2 ....
-	 * 
+	 *
 	 * The parameter names and values are url-encoded by this method.
-	 * 
+	 *
 	 * Note: Use redirect only if you need the client browser's URL to have a new URL.
 	 *       Its <strong>much faster</strong> to create a new instance of controllerClass, invoke the required action's method and return it's result.
-	 * 
+	 *
 	 * @param controllerClass
 	 * @param action can be null to use default action
 	 * @param parameterMap A map of parameter/value pairs.
 	 * @return A Target object
-	 */	
+	 */
 	@SuppressWarnings("rawtypes")
 	private Target internalRedirectToAction(Class<? extends BaseController> controllerClass, String action, Map<Object, Object> parameterMap) {
 		Controller annotation = controllerClass.getAnnotation(Controller.class);
@@ -242,17 +245,19 @@ public class BaseController {
 					url.append(first ? "?" : "&");
 					first = false;
 					Map.Entry entry = (Map.Entry)it.next();
-					url.append(URLEncoder.encode(entry.getKey().toString(), "UTF-8"))
-						.append("=")
-						.append(URLEncoder.encode(entry.getValue().toString(), "UTF-8"));
+					url.append(URLEncoder.encode(entry.getKey().toString(), "UTF-8"));
+					url.append("=");
+					if (entry.getValue() != null) {
+						url.append(URLEncoder.encode(entry.getValue().toString(), "UTF-8"));
+					}
 				}
 			}
 		} catch (UnsupportedEncodingException e) {
 			assert false : "No UTF-8 encoding supported?! Really?!";
 		}
 		return redirect(url.toString());
-	}	
-	
+	}
+
 	/**
 	 * If no table with specified key exists in current session, a new entry with the specified initialTable is created
 	 * and returned, otherwise the already existing table is returned.
@@ -267,7 +272,7 @@ public class BaseController {
 			table = initialTable;
 			tableCtrl.addTable(initialTable);
 		} else {
-			/* table model is intentionally transient, so it might be null even if the table is still there, 
+			/* table model is intentionally transient, so it might be null even if the table is still there,
 			 * after a session got restored. In that case, we take the model from the initialTable.
 			 */
 			if (table.getModel() == null) {
@@ -277,7 +282,7 @@ public class BaseController {
 		context.put(table.getKey(), table);
 		return table;
 	}
-	
+
 	/**
 	 * If no tree with specified key exists in current session, a new entry with the specified initialTree is created
 	 * and returned, otherwise the already existing tree is returned.
@@ -291,7 +296,7 @@ public class BaseController {
 			tree = initialTree;
 			trees.addTree(initialTree);
 		} else {
-			/* tree model is intentionally transient, so it might be null even if the tree is still there, 
+			/* tree model is intentionally transient, so it might be null even if the tree is still there,
 			 * after a session got restored. In that case, we take the model from the initialTree.
 			 */
 			if (tree.getModel() == null) {
