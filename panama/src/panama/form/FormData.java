78853cae9397b7f9790ba0406f3aae9734bdf4bd
robert.brandner@gmail.com <robert.brandner@gmail.com@11ccd14b-a046-9c1e-6379-9f16e458a607>
2012-12-27 20:23:45 +0000

    version 3.4.2 - bugfixes in PersistentBeanField, kleine Anpassung bei FormData.applyTo()

--- panama/src/panama/form/FormData.java
+++ panama/src/panama/form/FormData.java
@@ -1,17 +1,17 @@
 /*
- *  Copyright 2004-2010 Robert Brandner (robert.brandner@gmail.com) 
- *  
- *  Licensed under the Apache License, Version 2.0 (the "License"); 
- *  you may not use this file except in compliance with the License. 
- *  You may obtain a copy of the License at 
- *  
- *  http://www.apache.org/licenses/LICENSE-2.0 
- *  
- *  Unless required by applicable law or agreed to in writing, software 
- *  distributed under the License is distributed on an "AS IS" BASIS, 
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- *  See the License for the specific language governing permissions and 
- *  limitations under the License. 
+ *  Copyright 2004-2010 Robert Brandner (robert.brandner@gmail.com)
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 package panama.form;
 
@@ -39,19 +39,19 @@ import panama.util.DynaBeanUtils;
  * This class represents the data contained in the form passed to the constructor.
  * The form itself is just a description of a form and it's field and resides in the
  * application scope. It must never be changed by this class.
- * 
- * This FormData class contains the data and normally resides in request or session scope. 
- * 
+ *
+ * This FormData class contains the data and normally resides in request or session scope.
+ *
  * Validation is done during getting the values from the form.
- * 
- * This class does not support Multipart Fileuploads, instead, 
- * please use the FileItem methods of Context class to access uploaded files. 
- * 
+ *
+ * This class does not support Multipart Fileuploads, instead,
+ * please use the FileItem methods of Context class to access uploaded files.
+ *
  * This class provides getters for common types. On error (cast etc.) they simply return null
  * For how to get values of other types
  * @see FormData#getValue(String)
  * @see FormData#getValues(String)
- * 
+ *
  * @author Ridcully
  *
  */
@@ -60,8 +60,8 @@ public class FormData {
 	private Map<String, Object> input = new HashMap<String, Object>();
 	private Map<String, ValidatorException> errors = new HashMap<String, ValidatorException>();
 	private Form form;
-	
-	protected static SimpleLogger log = new SimpleLogger(Form.class);		
+
+	protected static SimpleLogger log = new SimpleLogger(Form.class);
 
 	/**
 	 * Invokes {@link #FormData(Form, Map)} with null for inputMap
@@ -70,14 +70,14 @@ public class FormData {
 	public FormData(Form form) {
 		this(form, null);
 	}
-	
+
 	/**
 	 * Presets input for all boolean fields with FALSE (as normally these are used for checkboxes
 	 * and we do not get values for unchecked checkboxes) before applying the passed inputMap.
 	 * If you do not want this behaviour, you can call setInput(field, null) afterwards for the fields
 	 * you do not want to have FALSE set as input (e.g. if you have a Boolean field and some sort of threeway input
 	 * with TRUE, FALSE and "don't care")
-	 * 
+	 *
 	 * @param form
 	 * @param inputMap
 	 */
@@ -106,8 +106,8 @@ public class FormData {
 	 * String[] arrays as coming from request (even if there's only one value)
 	 *
 	 * @see #setInput(String, Object)
-	 * 
-	 * @param inputMap 
+	 *
+	 * @param inputMap
 	 */
 	@SuppressWarnings("unchecked")
 	public void setInput(Map inputMap) {
@@ -120,7 +120,7 @@ public class FormData {
 
 	/**
 	 * Sets the form's input from the specified bean.
-	 * 
+	 *
 	 * @param bean The bean from which the values are fetched
 	 */
 	public void setInput(Object bean) {
@@ -133,16 +133,16 @@ public class FormData {
 			}
 		}
 	}
-	
+
 	/**
 	 * Sets the input value for one field
-	 * 
+	 *
 	 * If value is an object or array of objects, the objects are converted to strings
 	 * using the valueToString method of the field
-	 * 
+	 *
 	 * Note, that the value is not validated here and now, but just when it is fetched using
 	 * one of the get[String|Integer|...]() or the getValue() or getValues() methods.
-	 * 
+	 *
 	 * @param fieldName
 	 * @param value may be single string or object or an array of strings or objects
 	 */
@@ -158,7 +158,7 @@ public class FormData {
 			Field f = (Field)form.getFields().get(fieldName);
 			if (f != null) {
 				try {
-					input.put(fieldName, f.valuesToStrings((Object[])value)); 
+					input.put(fieldName, f.valuesToStrings((Object[])value));
 				} catch (Exception ex) {
 					input.put(fieldName, value); // could not convert, leave as is
 				}
@@ -168,59 +168,54 @@ public class FormData {
 		} else {
 			input.put(fieldName, value);
 		}
-	}	
+	}
 
 	// ----------------------------------------------------------------------------
 
 	/**
 	 * Applies current input-data to the specified bean.
 	 * The field names must match the property names for this method to work as expected.
-	 * If unknown properties are encountered, _no_ values are set (an error message is logged at debug-level)
-	 * If wrong property-types are encountered, or fields are missing for property-names a message is logged at debug-level.
+	 * If wrong property-types are encountered, or fields are missing for property-names a message is logged at warn-level.
 	 * @param bean The bean, the current input-data will be applied to.
 	 */
 	public void applyTo(Object bean) {
 		applyTo(bean, Form.EXCLUDE_PROPERTIES);
 	}
-	
+
 	/**
 	 * Applies current input-data to the specified properties of the specified bean.
 	 * The field names must match the property names for this method to work as expected.
-	 * If unknown properties are encountered, _no_ values are set (an error message is logged at debug-level)
-	 * If wrong property-types are encountered, or fields are missing for property-names a message is logged at debug-level.
+	 * If wrong property-types are encountered, or fields are missing for property-names a message is logged at warn-level.
 	 * @param bean The bean, the input should be applied to.
 	 * @param properties the properties to be set
 	 */
 	public void applyTo(Object bean, String... properties) {
 		applyTo(bean, Form.INCLUDE_PROPERTIES, properties);
-	}	
+	}
 
 	/**
 	 * Applies current input-data to the specified or all but the properties of the specified bean.
 	 * The field names must match the property names for this method to work as expected.
-	 * If unknown properties are encountered, _no_ values are set (an error message is logged at debug-level)
-	 * If wrong property-types are encountered, or fields are missing for property-names a message is logged at debug-level.
-	 * If a property is annotated as javax.persistence.Version it is NOT set!
-	 * 
+	 * If wrong property-types are encountered, or fields are missing for property-names a message is logged at warn-level.
+	 * Properties annotated as javax.persistence.Version are NOT set!
+	 *
 	 * @param bean The bean, the input should be applied to.
 	 * @param properties the properties to be set or to be skipped, depending on method
-	 * @param method wether to set the specified or all _but_ the specified properties. Value should be one of Form.INCLUDE_PROPERTIES or Form.EXCLUDE_PROPERTIES 
+	 * @param method wether to set the specified or all _but_ the specified properties. Value should be one of Form.INCLUDE_PROPERTIES or Form.EXCLUDE_PROPERTIES
 	 */
 	public void applyTo(Object bean, int method, String... properties) {
 		List<String> allProperties = Arrays.asList(DynaBeanUtils.getPropertyNames(bean));
 		List<String> props = Arrays.asList(properties == null ? new String[0] : properties);
-		if (!allProperties.containsAll(props)) {
-			List<String> hlp = new ArrayList<String>(props); 	// must create a real list here, the 'asList' arrays do not support the removeAll method
-			hlp.removeAll(allProperties);						// remove all existing properties --> the non existing stay in props.
-			log.debug("Class "+bean.getClass()+" does not contain the specified properties "+hlp);
-			return;
-		}
 		if (method == Form.EXCLUDE_PROPERTIES) {
 			List<String> hlp = new ArrayList<String>(allProperties);	// must create a real list here, the 'asList' arrays do not support the removeAll method
 			hlp.removeAll(props);
 			props = hlp;
 		}
 		for (String propertyName : props) {
+			if (!allProperties.contains(propertyName)) {
+				log.warn("Class "+bean.getClass()+" does not contain the specified property "+propertyName+"; skipping this property.");
+				continue;
+			}
 			try {
 				Class<?> propertyClass = DynaBeanUtils.getPropertyClass(bean, propertyName);
 				if (propertyClass.isArray()) {
@@ -234,18 +229,18 @@ public class FormData {
 				}
 			} catch (Exception e) {
 				/* NOP - continue; the error is already added to the errors-list. */
-			}  
+			}
 		}
 	}
-	
+
 	// ----------------------------------------------------------------------------
-	// getters for common types - on error (cast etc.) they simply return null 
+	// getters for common types - on error (cast etc.) they simply return null
 	// for non primitive types or a safe default value for primitive types.
 	// for how to get values of other types
 	// @see FormData#getValue()
 	// @see FormData#getValues()
 	// ----------------------------------------------------------------------------
-	
+
 	public String getString(String fieldName) {
 		try {
 			return (String)getValue(fieldName);
@@ -269,7 +264,7 @@ public class FormData {
 			return (Long)safeNullValue(fieldName);
 		}
 	}
-	
+
 	public Float getFloat(String fieldName) {
 		try {
 			return (Float)getValue(fieldName);
@@ -285,31 +280,31 @@ public class FormData {
 			return (Double)safeNullValue(fieldName);
 		}
 	}
-		
+
 	public Date getDate(String fieldName) {
 		try {
 			return (Date)getValue(fieldName);
 		} catch (Exception e) {
 			return (Date)safeNullValue(fieldName);
-		}		
+		}
 	}
-	
+
 	public Boolean getBoolean(String fieldName) {
 		try {
 			return (Boolean)getValue(fieldName);
 		} catch (Exception e) {
 			return (Boolean)safeNullValue(fieldName);
-		}		
+		}
 	}
-	
+
 	public FileItem getFileItem(String fieldName) {
 		try {
 			return (FileItem)getValue(fieldName);
 		} catch (Exception e) {
 			return null;
-		}		
+		}
 	}
-	
+
 	public String[] getStrings(String fieldName) {
 		try {
 			return (String[])castArrayType(String.class, getValues(fieldName));
@@ -333,7 +328,7 @@ public class FormData {
 			return null;
 		}
 	}
-	
+
 	public Float[] getFloats(String fieldName) {
 		try {
 			return (Float[])castArrayType(Float.class, getValues(fieldName));
@@ -349,45 +344,45 @@ public class FormData {
 			return null;
 		}
 	}
-		
+
 	public Date[] getDates(String fieldName) {
 		try {
 			return (Date[])castArrayType(Date.class, getValues(fieldName));
 		} catch (Exception e) {
 			return null;
-		}		
+		}
 	}
-	
+
 	public Boolean[] getBooleans(String fieldName) {
 		try {
 			return (Boolean[])castArrayType(Boolean.class, getValues(fieldName));
 		} catch (Exception e) {
 			return null;
-		}		
+		}
 	}
-	
+
 	public FileItem[] getFileItems(String fieldName) {
 		try {
 			return (FileItem[])castArrayType(FileItem.class, getValues(fieldName));
 		} catch (Exception e) {
 			return null;
-		}		
+		}
 	}
-	
+
 	/* converts Object[] array to array of specified componentType */
 	private Object castArrayType(Class<?> componentType, Object[] array) {
 		Object castArray = Array.newInstance(componentType, array.length);
 		System.arraycopy(array, 0, castArray, 0, array.length);
 		return castArray;
-	}		
-	
+	}
+
 	/**
 	 * Gets the raw value object for the specified field.
 	 * @param fieldName
 	 * @return Raw value object as a single object.
 	 * @throws NoSuchFieldException
 	 */
-	public Object getValue(String fieldName) throws NoSuchFieldException {		
+	public Object getValue(String fieldName) throws NoSuchFieldException {
 		Object[] values = getValues(fieldName);
 		if (values != null && values.length > 0) {
 			return values[0];
@@ -407,7 +402,7 @@ public class FormData {
 			log.warn("no field named '"+fieldName+"'");
 			return null;
 		}
-		if (field.getValueClass().isPrimitive()) {	
+		if (field.getValueClass().isPrimitive()) {
 			return DynaBeanUtils.getNullValueForPrimitive(field.getValueClass());
 		} else {
 			return null;
@@ -419,8 +414,8 @@ public class FormData {
 	 * @param fieldName
 	 * @return Raw value objects as object array.
 	 * @throws NoSuchFieldException
-	 */	
-	public Object[] getValues(String fieldName) throws NoSuchFieldException {		
+	 */
+	public Object[] getValues(String fieldName) throws NoSuchFieldException {
 		Field field = (Field)form.getFields().get(fieldName);
 		if (field == null) {
 			throw new NoSuchFieldException("No field named '"+fieldName+"'");
@@ -476,12 +471,12 @@ public class FormData {
 			}
 		}
 		return result;
-	}	
-	
+	}
+
 	/**
 	 * Sets value(s) for specified field from input map
 	 * This invokes the fields validators, validation errors are added to error map
-	 * 
+	 *
 	 * @param f
 	 * @param values An array of strings, array of objects or null
 	 */
@@ -498,16 +493,16 @@ public class FormData {
 				} catch (ParseException pe) {
 					String msg = Context.getInstance().getLocalizedString(Validator.PARSING_FAILED);
 					throw new ValidatorException(msg, pe);
-				}				
+				}
 			} else {
-				return validateValues(f, (Object[])values);					
+				return validateValues(f, (Object[])values);
 			}
 		} catch (ValidatorException ve) {
 			errors.put(f.getName(), ve);
 			return (Object[])values;
-		}				
-	}	
-	
+		}
+	}
+
 	/**
 	 * Sets values of this field after validating
 	 * @param values An array of Objects (most of the time just one element)
@@ -515,20 +510,20 @@ public class FormData {
 	private Object[] validateValues(Field f, Object[] values) throws ValidatorException {
 		f.validate(values);	// throws ValidatorException on error
 		return values;
-	}	
-	
+	}
+
 	// ----------------------------------------------------------------------------
-	
+
 	/**
 	 * Gets the original input for the specified field.
 	 * Useful to fill a field where validation failed.
-	 * 
-	 * Intentionally named just get() as this allows 
-	 * to use $formdata.fieldName in velocity which invokes 
+	 *
+	 * Intentionally named just get() as this allows
+	 * to use $formdata.fieldName in velocity which invokes
 	 * this method as form.get('fieldName')
-	 * 
+	 *
 	 * If (as it is most of the time) there is just one value, the array is flattened
-	 * 
+	 *
 	 * @param fieldName
 	 * @return the original input
 	 */
@@ -541,7 +536,7 @@ public class FormData {
 			return o;
 		}
 	}
-	
+
 	/**
 	 * Provides a copy of the input map.
 	 * To be used mainly with JSPs with JSTL as ${formdata.input["fieldName"]}
@@ -561,12 +556,12 @@ public class FormData {
 		}
 		return m;
 	}
-	
+
 	/**
 	 * Gets the original input for the specified field.
 	 * This method does not flatten the input array if there's only one value.
 	 * Useful if you need to iterate over the input, regardless whether it has one or more values.
-	 * 
+	 *
 	 * @param fieldName
 	 * @return the original input
 	 */
@@ -574,10 +569,10 @@ public class FormData {
 		Object[] o = (Object[])input.get(fieldName);
 		if (o == null) { o = new Object[0]; }
 		return o;
-	}	
-	
+	}
+
 	// ----------------------------------------------------------------------------
-	
+
 	/**
 	 * Gets map of errors, key == fieldname, value = ValidatorException
 	 * @return A map of errors or an empty map
@@ -585,7 +580,7 @@ public class FormData {
 	public Map<String, ValidatorException> getErrors() {
 		return errors;
 	}
-	
+
 	/**
 	 * Sets error message for the specified field.
 	 * An already existing error message will be overridden by calling this methd.
@@ -595,7 +590,7 @@ public class FormData {
 	public void setError(String fieldName, String errorMessage) {
 		errors.put(fieldName, new ValidatorException(errorMessage));
 	}
-	
+
 	/**
 	 * Checks if there are errors
 	 * @return true if there are errors, false otherwise
@@ -603,19 +598,19 @@ public class FormData {
 	public boolean hasErrors() {
 		return errors.size() > 0;
 	}
-	
+
 	// ----------------------------------------------------------------------------
 	// Helper methods
 	// ----------------------------------------------------------------------------
-	
+
 	public void clearInput() {
 		input = new HashMap<String, Object>();
 	}
-	
+
 	/**
 	 * Clears error map
 	 */
 	public void clearErrors() {
-		errors = new HashMap<String, ValidatorException>();		
+		errors = new HashMap<String, ValidatorException>();
 	}
 }
--- panama/src/panama/form/PersistentBeanField.java
+++ panama/src/panama/form/PersistentBeanField.java
@@ -1,17 +1,17 @@
 /*
- *  Copyright 2004-2010 Robert Brandner (robert.brandner@gmail.com) 
- *  
- *  Licensed under the Apache License, Version 2.0 (the "License"); 
- *  you may not use this file except in compliance with the License. 
- *  You may obtain a copy of the License at 
- *  
- *  http://www.apache.org/licenses/LICENSE-2.0 
- *  
- *  Unless required by applicable law or agreed to in writing, software 
- *  distributed under the License is distributed on an "AS IS" BASIS, 
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- *  See the License for the specific language governing permissions and 
- *  limitations under the License. 
+ *  Copyright 2004-2010 Robert Brandner (robert.brandner@gmail.com)
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 package panama.form;
 
@@ -26,18 +26,18 @@ import panama.persistence.PersistentBean;
  * The string-representation in FormData is just the ID of the object (so meaningful data has to be fetched from elsewhere)
  * This field also works with collections, but they have to be converted to/from the arrays used in FormData.
  * Note, that this field accesses the DataBase (via Ebean), so this must be set up and initialized correctly.
- * 
+ *
  * @author Ridcully
  *
  */
 public class PersistentBeanField extends Field {
 
-	protected Class<? extends PersistentBean> beanClass; 
+	protected Class<? extends PersistentBean> beanClass;
 
 	public PersistentBeanField(String name, Class<? extends PersistentBean> beanClass) {
 		this(name, false, beanClass);
-	}	
-	
+	}
+
 	/**
 	 * Constructor of the Field
 	 * @param name
@@ -48,21 +48,22 @@ public class PersistentBeanField extends Field {
 		super(name, beanClass, notEmpty);
 		this.beanClass = beanClass;
 	}
-	
+
 	/**
 	 * This converts the given string into the value itself
 	 * @see Field#stringToValue(String)
 	 * @param valueString the id of an PersistentBean
-	 * @return The PersistentBean (or an object of beanClass if specified by Constructor) for the specified id, or a new object if no persistent object for the id is found 
+	 * @return The PersistentBean (or an object of beanClass if specified by Constructor) for the specified id, or a new object if no persistent object for the id is found
 	 * @throws ParseException if something goes wrong
 	 */
+	@Override
 	protected Object stringToValue(String valueString) throws ParseException {
 		try {
 			return PersistentBean.findOrCreate(beanClass, valueString);
 		} catch (Exception e) {
 			throw new ParseException(e.getMessage(), 0);
 		}
-	}	
+	}
 
 	/**
 	 * This converts the given value into a string representation
@@ -70,8 +71,13 @@ public class PersistentBeanField extends Field {
 	 * @param value
 	 * @return The ID of value
 	 * @throws ParseException
-	 */	
-	public String valueToString(PersistentBean value) throws ParseException {
-		return value.getId();
-	}	
+	 */
+	@Override
+	public String valueToString(Object value) throws ParseException {
+		try {
+			return ((PersistentBean)value).getId();
+		} catch (Exception e) {
+			throw new ParseException(e.getMessage(), 0);
+		}
+	}
 }
