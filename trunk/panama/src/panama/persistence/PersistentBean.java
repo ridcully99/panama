4354efa3fb1bbef1daa536c1f57efbc1a9d921ae
robert.brandner@gmail.com <robert.brandner@gmail.com@11ccd14b-a046-9c1e-6379-9f16e458a607>
2013-02-09 19:50:13 +0000

    find current class from static does not work as intended

--- trunk/panama/src/panama/persistence/PersistentBean.java
+++ trunk/panama/src/panama/persistence/PersistentBean.java
@@ -1,17 +1,17 @@
 /*
- *  Copyright 2004-2012 Robert Brandner (robert.brandner@gmail.com) 
- *  
- *  Licensed under the Apache License, Version 2.0 (the "License"); 
- *  you may not use this file except in compliance with the License. 
- *  You may obtain a copy of the License at 
- *  
- *  http://www.apache.org/licenses/LICENSE-2.0 
- *  
- *  Unless required by applicable law or agreed to in writing, software 
- *  distributed under the License is distributed on an "AS IS" BASIS, 
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- *  See the License for the specific language governing permissions and 
- *  limitations under the License. 
+ *  Copyright 2004-2012 Robert Brandner (robert.brandner@gmail.com)
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 package panama.persistence;
 
@@ -29,7 +29,7 @@ import com.avaje.ebean.Ebean;
 
 /**
  * A basic bean for persistence, that provides a unique ID and matching equals() and hashCode() methods.
- * 
+ *
  * @author Ridcully
  */
 @MappedSuperclass
@@ -43,7 +43,7 @@ public class PersistentBean implements Serializable {
 	 */
 	@Id @Column(length=64)
 	protected String id = new VMID().toString().replace(':', 'x');	// replacing colons as they are not allowed for id attributes in HTML tags.
-	
+
 	/**
 	 * A version field
 	 * Using the timestamp you can easily check if an object is new or persistent by checking if timestamp is null
@@ -53,7 +53,7 @@ public class PersistentBean implements Serializable {
 
 	public PersistentBean() {
 	}
-	
+
 	public void setId(String id) {
 		this.id = id;
 	}
@@ -69,14 +69,14 @@ public class PersistentBean implements Serializable {
 	public void setTimeStamp(Date timeStamp) {
 		this.timeStamp = timeStamp;
 	}
-	
+
 	/**
-	 * Tries to fetch an object of type beanType with specified id from database (using Ebean). 
+	 * Tries to fetch an object of type beanType with specified id from database (using Ebean).
 	 * If no such object is found, a new one is created and it's id property set to the specified id.
 	 * This newly created object is _not_ saved to the database.
 	 * You can detect wether an object is new or came from the database by checking it's timeStamp property,
 	 * which is null for unsaved objects and not null for saved ones.
-	 * 
+	 *
 	 * @param beanType
 	 * @param id
 	 * @return an object of class beanType; never null.
@@ -98,47 +98,21 @@ public class PersistentBean implements Serializable {
 		}
 		return o;
 	}
-	
-	/**
-	 * Tries to fetch an object of type of class the method is called upon, with specified id from database (using Ebean). 
-	 * If no such object is found, a new one is created and it's id property set to the specified id.
-	 * This newly created object is _not_ saved to the database.
-	 * You can detect wether an object is new or came from the database by checking it's timeStamp property,
-	 * which is null for unsaved objects and not null for saved ones.
-	 * 
-	 * Finding current class in static method is taken from here:
-	 * http://www.rgagnon.com/javadetails/java-0402.html
-	 * 
-	 * @param beanType
-	 * @param id
-	 * @return an object of class beanType; never null.
-	 */
-	@SuppressWarnings("unchecked")
-	public static <T extends PersistentBean> T findOrCreate(String id) {
-		Class<T> beanType = (Class<T>)new CurrentClassGetter().getCurrentClass();
-		return findOrCreate(beanType, id);
-	}
 
-	private static class CurrentClassGetter extends SecurityManager {
-		public Class<?> getCurrentClass() {
-			return getClassContext()[1];
-		}
-	}
-	
 	/**
-	 * equals - based on id 
+	 * equals - based on id
 	 */
 	public boolean equals(Object other) {
 		if (this == other) { return true; }
 		if ((other == null) || (other.getClass() != this.getClass() ) ) return false;
 		return id.equals(((PersistentBean)other).getId());
 	}
-	
+
 	/**
 	 * hashcode - based on id
 	 */
 	public int hashCode() {
 		return id.hashCode();
-	} 
-	
+	}
+
 }
