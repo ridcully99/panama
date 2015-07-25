184bea160432b1e810d89c4d3bc81abe58269dd4
ridcully99 <robert.brandner@gmail.com>
2015-07-25 14:51:59 +0200

    UUIDGenerator verbessert um sicher zu sein dass negative und positive unterschiedlich encoded

--- panama/src/panama/util/UUIDGenerator.java
+++ panama/src/panama/util/UUIDGenerator.java
@@ -8,27 +8,35 @@ import java.util.UUID;
 /**
  * Generates Base62 encoded RandomUUIDs for use as IDs in PersistentBean etc.
  * Using https://gist.github.com/jdcrensh/4670128
+ * Negative values are encoded like positive with special character 0 which is not in the alphabet prepended.
+ * All UUIDs are padded at the end with special character 0 which is not in the alphabet to 24 characters.
  */
 public class UUIDGenerator {
 
-	public static final String ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	private static final String ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789";
+	private static final char SPECIAL = '0';
 
 	public static final int BASE = ALPHABET.length();
 
 	private UUIDGenerator() {}
-	
+
 	public static String getUUID() {
 		final UUID uuid = UUID.randomUUID();
 		StringBuilder sb = new StringBuilder();
 		append(sb, uuid.getLeastSignificantBits());
 		append(sb, uuid.getMostSignificantBits());
+		while(sb.length() < 24) sb.append(SPECIAL);
 		return sb.toString();
 	}
 
 	public static void append(StringBuilder sb, long i) {
-		while (i != 0) {
+		if (i < 0) {
+			sb.append(SPECIAL);
+			i = Math.abs(i);
+		}
+		while (i > 0) {
 			final int rem = (int) (i % BASE);
-			sb.append(ALPHABET.charAt(Math.abs(rem)));
+			sb.append(ALPHABET.charAt(rem));
 			i /= BASE;
 		}
 	}
--- panama/tests/panama/tests/UUIDTest.java
+++ panama/tests/panama/tests/UUIDTest.java
@@ -1,17 +1,17 @@
 /*
- *  Copyright 2004-2015 Robert Brandner (robert.brandner@gmail.com) 
- *  
- *  Licensed under the Apache License, Version 2.0 (the "License"); 
- *  you may not use this file except in compliance with the License. 
- *  You may obtain a copy of the License at 
- *  
- *  http://www.apache.org/licenses/LICENSE-2.0 
- *  
- *  Unless required by applicable law or agreed to in writing, software 
- *  distributed under the License is distributed on an "AS IS" BASIS, 
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- *  See the License for the specific language governing permissions and 
- *  limitations under the License. 
+ *  Copyright 2004-2015 Robert Brandner (robert.brandner@gmail.com)
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 package panama.tests;
 
@@ -29,27 +29,29 @@ import panama.util.UUIDGenerator;
 public class UUIDTest extends TestCase {
 
 	private final static int TIMES = 10000;
-	
+
 	public void testPerformance() {
 		long vmidMillis = doVMID();
 		long uuidGeneratorMillis = doUUIDGenerator();
 		System.out.println(vmidMillis);
 		System.out.println(uuidGeneratorMillis);
 	}
-	
+
 	public void testUUIDGenerator() {
 		for (int i = 0; i < 10; i++) {
 			System.out.println(i + " : " + UUIDGenerator.getUUID());
 		}
 	}
-	
+
 	public void testUUIDGenerator_Uniqueness() {
 		Set<String> values = new HashSet<String>();
-		for (int i = 0; i < TIMES * 1000; i++) {
+		final int n = TIMES * 100;
+		for (int i = 0; i < n; i++) {
 			String uuid = UUIDGenerator.getUUID();
 			assertFalse(values.contains(uuid));
 			values.add(uuid);
 		}
+		System.out.println(n + " generated - all unique");
 	}
 
 	private long doVMID() {
