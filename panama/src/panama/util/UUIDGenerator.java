62d8fd6f38eca75e20fd2737bc8e8f5f0cd33972
ridcully99 <robert.brandner@gmail.com>
2015-07-27 11:13:13 +0200

    Docu corrected

--- panama/src/panama/util/UUIDGenerator.java
+++ panama/src/panama/util/UUIDGenerator.java
@@ -8,8 +8,7 @@ import java.util.UUID;
 /**
  * Generates Base62 encoded RandomUUIDs for use as IDs in PersistentBean etc.
  * Using https://gist.github.com/jdcrensh/4670128
- * Negative values are encoded like positive with special character 0 which is not in the alphabet prepended.
- * All UUIDs are padded at the end with special character 0 which is not in the alphabet to 24 characters.
+ * All UUIDs are 24 characters long.
  */
 public class UUIDGenerator {
 
