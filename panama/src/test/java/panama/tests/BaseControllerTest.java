e03a3dae21e1040e69299cea3947fb01db4c79b2
ridcully99 <robert.brandner@gmail.com>
2016-06-16 14:51:08 +0200

    Updated header comments

--- panama/src/test/java/panama/tests/BaseControllerTest.java
+++ panama/src/test/java/panama/tests/BaseControllerTest.java
@@ -1,17 +1,17 @@
 /*
- *  Copyright 2004-2012 Robert Brandner (robert.brandner@gmail.com) 
- *  
- *  Licensed under the Apache License, Version 2.0 (the "License"); 
- *  you may not use this file except in compliance with the License. 
- *  You may obtain a copy of the License at 
- *  
- *  http://www.apache.org/licenses/LICENSE-2.0 
- *  
- *  Unless required by applicable law or agreed to in writing, software 
- *  distributed under the License is distributed on an "AS IS" BASIS, 
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- *  See the License for the specific language governing permissions and 
- *  limitations under the License. 
+ *  Copyright 2004-2016 Robert Brandner (robert.brandner@gmail.com)
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 package panama.tests;
 
@@ -23,7 +23,7 @@ public class BaseControllerTest extends ContextTestCase {
 	public void testRenderPathCreation() {
 		BaseController ctrl = new BaseController();
 		String abs = "/panama/core/template.vm";
-		
+
 		assertEquals(abs, ((TemplateTarget)ctrl.render(abs)).getTemplate());
 		assertEquals(abs, ((TemplateTarget)ctrl.render("template.vm")).getTemplate());
 		assertEquals(abs, ((TemplateTarget)ctrl.render("./template.vm")).getTemplate());
