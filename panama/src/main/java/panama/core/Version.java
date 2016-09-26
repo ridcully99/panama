77ffc320897af217fa6d90bb226d2abdd078677e
ridcully99 <robert.brandner@gmail.com>
2016-09-26 11:35:38 +0200

    Java 1.7; Version 4.0.1

--- panama/src/main/java/panama/core/Version.java
+++ panama/src/main/java/panama/core/Version.java
@@ -1,17 +1,17 @@
 /*
- *  Copyright 2004-2016 Robert Brandner (robert.brandner@gmail.com) 
- *  
- *  Licensed under the Apache License, Version 2.0 (the "License"); 
- *  you may not use this file except in compliance with the License. 
- *  You may obtain a copy of the License at 
- *  
- *  http://www.apache.org/licenses/LICENSE-2.0 
- *  
- *  Unless required by applicable law or agreed to in writing, software 
- *  distributed under the License is distributed on an "AS IS" BASIS, 
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- *  See the License for the specific language governing permissions and 
- *  limitations under the License. 
+ *  Copyright 2004-2016 Robert Brandner (robert.brandner@gmail.com)
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 package panama.core;
 
@@ -27,7 +27,7 @@ public class Version {
 	// b: Revision (increases when cool new features are added)
 	// c: Minor	(increases when updates or fixes are made - even numbers mark stable releases)
 
-	public final static String VERSION = "4.0.0 beta";
+	public final static String VERSION = "4.0.1";
 
 	public final static String LOGO_ASCIIART =
 	" ______ _______ _______ _______ _______ _______ \n"+
@@ -40,6 +40,7 @@ public class Version {
 
 	/* 	History
 	 * ---------------------------------------------------------------------------------------------
+	 * 4.0.1    2016-09-26 Switched to Java 1.7 for better compatibility
 	 * 4.0.0    2106-xx-yy Get rid off servlet.jar, switch to dynamic ebean enhancement, switch to gradle build
 	 * 3.9.2    2015-12-26 Throwing HttpErrorException from within method did not work properly. Fixed.
 	 * 3.9.1    2015-08-09 Added Target#setStatusCode() method
